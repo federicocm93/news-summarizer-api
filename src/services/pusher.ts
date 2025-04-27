b7f32ce4cce408db5f1bbbaec9b81801d08ffb38
federico <federico@rebill.com>
2025-04-27 20:23:07 -0300

    fix pusher env variables

--- src/services/pusher.ts
+++ src/services/pusher.ts
@@ -2,14 +2,18 @@ import Pusher from "pusher";
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID || "",
-  key: process.env.PUSHER_KEY || "",
-  secret: process.env.PUSHER_SECRET || "",
+  key: process.env.PUSHER_APP_KEY || "",
+  secret: process.env.PUSHER_APP_SECRET || "",
   cluster: "us2",
   useTLS: true
 });
 
 
 export const triggerNewSubscriptionPushEvent = async (userId: string) => {
+  if (!userId) {
+    console.warn("Cannot trigger new subscription push event: No user ID provided");
+    return;
+  }
   pusher.trigger("user-subscription-channel", "new-subscription", {
     userId
   });
